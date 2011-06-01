fbed23567817ec0316a6a7c6023401f09c87bd6d
Sabina Kowalczyk <sabkow@vp.pl>
2011-06-01 14:59:53 +0200

    tworzenie archiwum

--- kolo1.cpp
+++ kolo1.cpp
@@ -1,7 +1,16 @@
 // Sabina Kowalczyk, Zintergrowane systemy zarzadzania i analizy danych, 101496
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+void pytanie() 
+{	
+	cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;	
+	cout<<"A1) git HEAD^^^ HEAD^^"<<endl;
+	
+	cout<<"Q2) Co sie zmienilo?"<<endl;	
+	cout<<"A2) ..."<<endl;
+}
 
 #include <iostream>
 using namespace std;
@@ -12,9 +21,6 @@ int main(int argc, char **argv) {
 =======
   cout<<"I don't like git"<<endl;
 >>>>>>> cplusplus
-=======
-int main(int argc, char **argv) {
->>>>>>> parent of 7c034a7... funkcja z pytaniem
-  return 0;
+  pytanie();
 }
 
--- kolo1.java
+++
@@ -1,6 +0,0 @@
-
-class Kolo1 {
-  public static void main(String[] args) {
-  }
-}
-
